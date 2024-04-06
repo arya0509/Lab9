c6e323d47b5d5070751150d4a63cda240447d17e
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-04-05 18:28:26 -0600

    Connecting the database

--- CourseManager.cs
+++ CourseManager.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySqlConnector;
 namespace Lab9
 {
-    internal class CourseManager
+    
+    public class CourseManager
     {
+        private string connectionString;
+        public CourseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+            OpenConnection();
+        }
+
+        public void OpenConnection() 
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+            }
+            Console.WriteLine("Database connection successful");
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Lab9
+﻿using MySqlConnector;
+
+namespace Lab9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            MySqlConnectionStringBuilder builder
+                = new MySqlConnectionStringBuilder
+                {
+                    Server = "127.0.0.1",
+                    UserID = "root",
+                    Password = "root",
+                    Database = "students"
+                };
+            CourseManager courseManager = new CourseManager(builder.ConnectionString);
         }
     }
 }
